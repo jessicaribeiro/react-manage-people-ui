e599c4b524946b8c819ef1ff789bd9e0f1da5d24
Jessica Ribeiro <jessica.ribeiro@pipedrive.com>
2022-06-27 18:43:19 +0100

    fix: specify types for table columns used on header

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,19 @@ import './App.css';
 import Layout from "./components/Layout";
 import { Table as ApplicationsTable } from "./components/Table";
 import ApplicationsAPI from "./api/factories/applications-api";
-import { Candidate } from "./api/types";
 import { CircularProgress } from "@mui/material";
 import { css } from "@emotion/css";
+import { Candidate, SortKeys } from "./components/types";
+
+const tableColumns: {label: string, key: SortKeys, sortable: boolean}[] = [
+    { label: "Name", key: "name", sortable: false },
+    { label: "Email", key: "email", sortable: false },
+    { label: "Age", key: "birth_date", sortable: false },
+    { label: "Years of Experience", key: "year_of_experience", sortable: true },
+    { label: "Position applied", key: "position_applied", sortable: true },
+    { label: "Applied", key: "application_date", sortable: true },
+    { label: "Status", key: "status", sortable: false },
+];
 
 function App() {
     const [candidates, setCandidates] = useState<Candidate[]>([]);
@@ -36,7 +46,7 @@ function App() {
 
     return (
         <Layout>
-            <ApplicationsTable candidates={candidates} />
+            <ApplicationsTable columns={tableColumns} candidates={candidates} />
         </Layout>
     );
 }
