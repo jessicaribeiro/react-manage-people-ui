569c9b5cbfe679a312abbbb0eaca9013c9588881
Jessica Ribeiro <jessica.ribeiro@pipedrive.com>
2022-06-28 05:16:37 +0100

    feature: add router to save search parameters on url; create Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './App.css';
-import Layout from "./components/Layout";
-import { Table as ApplicationsTable } from "./components/Table";
-import ApplicationsAPI from "./api/factories/applications-api";
-import { CircularProgress } from "@mui/material";
-import { css } from "@emotion/css";
-import { Candidate, SortKeys } from "./components/types";
-
-const tableColumns: {label: string, key: SortKeys, sortable: boolean}[] = [
-    { label: "Name", key: "name", sortable: false },
-    { label: "Email", key: "email", sortable: false },
-    { label: "Age", key: "birth_date", sortable: false },
-    { label: "Years of Experience", key: "year_of_experience", sortable: true },
-    { label: "Position applied", key: "position_applied", sortable: true },
-    { label: "Applied", key: "application_date", sortable: true },
-    { label: "Status", key: "status", sortable: false },
-];
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/Home";
 
 function App() {
-    const [candidates, setCandidates] = useState<Candidate[]>([]);
-    const [isLoading, setIsLoading] = useState<boolean>(false);
-
-    useEffect(() => {
-        const fetchCandidates = async () => {
-            setIsLoading(true);
-
-            const response = await ApplicationsAPI.getAllCandidates();
-            let data = response.data;
-            setCandidates(data);
-
-            setIsLoading(false);
-        };
-
-        fetchCandidates();
-    }, []);
-
-
-    if (isLoading) {
-        return (
-            <div className={loadingStyle}>
-                <CircularProgress />
-            </div>
-        );
-    }
-
     return (
-        <Layout>
-            <ApplicationsTable columns={tableColumns} candidates={candidates} />
-        </Layout>
-    );
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+    )
 }
 
 export default App;
 
-
-const loadingStyle = css`
-  display: flex;
-  justify-content: center;
-  margin-top: 60px;
-`;
---
+++ src/components/Home.tsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from 'react';
+import { CircularProgress } from "@mui/material";
+import { Table as ApplicationsTable } from "./Table";
+import { css } from "@emotion/css";
+import { Candidate, SortKeys } from "./types";
+import ApplicationsAPI from "../api/factories/applications-api";
+import Layout from "./Layout";
+
+const tableColumns: { label: string, key: SortKeys, sortable: boolean }[] = [
+    { label: "Name", key: "name", sortable: false },
+    { label: "Email", key: "email", sortable: false },
+    { label: "Age", key: "birth_date", sortable: false },
+    { label: "Years of Experience", key: "year_of_experience", sortable: true },
+    { label: "Position applied", key: "position_applied", sortable: true },
+    { label: "Applied", key: "application_date", sortable: true },
+    { label: "Status", key: "status", sortable: false },
+];
+
+export function Home() {
+    const [candidates, setCandidates] = useState<Candidate[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    useEffect(() => {
+        const fetchCandidates = async () => {
+            setIsLoading(true);
+
+            const response = await ApplicationsAPI.getAllCandidates();
+            let data = response.data;
+            setCandidates(data);
+
+            setIsLoading(false);
+        };
+
+        fetchCandidates();
+    }, []);
+
+
+    if (isLoading) {
+        return (
+            <div className={loadingStyle}>
+                <CircularProgress />
+            </div>
+        );
+    }
+
+    return (
+        <Layout>
+            {candidates && <ApplicationsTable columns={tableColumns} candidates={candidates} />}
+        </Layout>
+    )
+}
+
+const loadingStyle = css`
+  display: flex;
+  justify-content: center;
+  margin-top: 60px;
+`;
